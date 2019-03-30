c121c52c3933d41fb044c5a2001eee297137f93a
Claudiiaa <47463136+Claudiiaa@users.noreply.github.com>
2019-03-30 20:42:42 +0000

    tried to add js to make carousel work

--- js/background.js
+++ js/background.js
@@ -4,6 +4,16 @@ console.log('hello!')
 // Only fires logic after the page has fully loaded
 $( document ).ready(function(){
 
-$('.carousel').carousel({
+$('.carousel').carousel(function(event){
   interval: 2000
+
+})
+
+$('#carousel').on('slide.bs.carousel', function () {
+
+    $('.carousel').slide.bs.carousel(function(event){
+    $('cycle')
+    $('pause')
+    $('prev')
+    $('next')
 })
