e221e81b3f75ad7ac5551513719680f596fdf6b3
Claudiiaa <47463136+Claudiiaa@users.noreply.github.com>
2019-03-30 20:17:55 +0000

    added javascript

---
+++ js/background.js
@@ -0,0 +1,9 @@
+// writes to the log 'hello'
+console.log('hello!')
+
+// Only fires logic after the page has fully loaded
+$( document ).ready(function(){
+
+$('.carousel').carousel({
+  interval: 2000
+})
